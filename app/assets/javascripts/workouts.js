9b7b1300006d63e815a10837ff8c9bb068c1acee
learn <learn@learn08.local>
2016-09-29 16:51:03 -0700

    added modal with testing, no time in modal yet

--- app/assets/javascripts/workouts.js
+++ app/assets/javascripts/workouts.js
@@ -11,6 +11,21 @@ $(document).ready(function() {
      slotMinutes: 15,
      events: "/workouts/get_workouts/",
      timeFormat: "LT",
-     dragOpacity: "0.5"
+     dragOpacity: "0.5",
+     eventClick:  function(event, jsEvent, view) {
+         $('#modalTitle').html(event.title);
+         $('#instructor').html(event.instructor);
+         $('#description').html(event.description);
+         $('#location').html(event.location);
+         $('#date').html(event.date);
+         $('#time').html(event.start);
+         $('#category').html(event.category);
+         $('#price').html(event.price);
+         $('#duration').html(event.duration);
+         $('#level').html(event.level);
+         $('#eventUrl').attr('href',event.url);
+         $('#fullCalModal').modal();
+         return false;
+     }
   });
 });
