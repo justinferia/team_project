d558e4516803e89156a857f7858995c0e3c391d4
William <tanwilliam90@gmail.com>
2016-10-06 15:11:03 -0700

    created some seeds for the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create! :email => "remy@pickles.com", :password => "password", :password_confirmation=> "password", :first_name => "Remy", :last_name => "Pickles", :id => "1"
+user = User.find_by_first_name("Remy")
+user.add_role("guest")
+
+user = User.create! :email => "alex@yong.com", :password => "password", :password_confirmation=> "password", :first_name => "Alex", :last_name => "Yong", :id => "2"
+user = User.find_by_first_name("Alex")
+user.add_role("instructor")
+
+user = User.create! :email => "justin@feria.com", :password => "password", :password_confirmation=> "password", :first_name => "Justin", :last_name => "Feria", :id => "3"
+user = User.find_by_first_name("Justin")
+user.add_role("instructor")
+
+workout = Workout.create! :name => "Traithlon Training", :date => "2016-10-07", :time => "12:00:00", :location => "3803 Ray St.", :category => "Cardio", :duration => "60 Minutes", :level => "Intermediate", :user_id => "3", :description => "Running, Swimming, Cycling"
+
+workout = Workout.create! :name => "Beach Yoga", :date => "2016-10-14", :time => "17:00:00", :location => "Huntington Beach", :category => "Yoga", :duration => "60 Minutes", :level => "Beginner", :user_id => "2", :description => "Yoga at the beach!"
+
+workout = Workout.create! :name => "Yoga X", :date => "2016-10-10", :time => "12:00:00", :location => "9155 Judicial Dr, San Diego, CA", :category => "Yoga", :duration => "60 Minutes", :level => "Intermediate", :user_id => "3", :description => "Yoga at the beach!"
+
+workout = Workout.create! :name => "Marathon Training", :date => "2016-10-12", :time => "15:00:00", :location => "Balboa Park, San Diego", :category => "Cardio", :duration => "90 Minutes", :level => "Advanced", :user_id => "2", :description => "RUN FORREST RUN"
