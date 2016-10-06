ff00b3753d3a521c7b2a6d7f4a14fa69266f8910
justinferia <justinferia@gmail.com>
2016-10-06 11:41:50 -0700

    added twitter, tested

---
+++ spec/features/twitter_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+#
+RSpec.feature "Twitter",js:true, type: :feature do
+  context "go to website and sign in with twitter" do
+    Steps 'Go to landing page' do
+      Given 'I am on the landing page' do
+        visit '/'
+      end
+      Then 'I click on sign in with twitter' do
+        click_link 'Sign in with Twitter'
+      end
+      Then 'fill out form' do
+        fill_in('username_or_email', :with=>"gameboy55115@gmail.com")
+        fill_in('password', :with=>'gamecube')
+        click_button 'allow'
+      end
+      Then 'i have successfully signed in' do
+        expect(page).to have_content("Successfully authenticated from Twitter account.")
+      end
+    end
+  end
+end
